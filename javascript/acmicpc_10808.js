4c22f99948a67409efb0ae0890efd04795ff40e6
minho.yoo <minho.yoo.dev@gmail.com>
2021-02-07 23:09:35 +0900

    add javascript

--- javascript/acmicpc_10808.js
+++ javascript/acmicpc_10808.js
@@ -1,15 +1,11 @@
-// const fs = require('fs');
-// const s = fs.readFileSync('/dev/stdin').toString()
-const s = 'backjun'
+const fs = require('fs');
+const s = fs.readFileSync('/dev/stdin').toString()
 const a = 'a'.charCodeAt(0), b = 'z'.charCodeAt(0);
 const result = []
 for (let i = a; i <= b; ++i) {
     result.push(0);
 }
-console.log(a)
 for (const c of s) {
-    console.log(c, c.charCodeAt(0))
-    console.log(c.charCodeAt(0) - a)
     result[c.charCodeAt(0) - a] += 1
 }
 
