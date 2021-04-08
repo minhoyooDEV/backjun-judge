14c753795f792862bb3402fd4f3c6582e8a4b8b5
myno <minho.yoo.dev@gmail.com>
2021-04-08 11:03:49 +0900

    Add pythons

--- python/yg_company/problem_2.py
+++ python/yg_company/problem_2.py
@@ -1,21 +1,38 @@
+from collections import defaultdict
 
 
-n = 125
+def func(l: list[int]):
 
+    # 누적값 저장
+    m = defaultdict(int)
+    zero_set = []
+    exceed_set = []
+    length = len(l)
 
-def func(n):
-    if n < 10:
-        return 0
+    for n in l:
+        m[n] += 1
 
-    result = 1
-    while n >= 10:
-        n = n // 10
-        result *= 10
+    print(m)
 
+    for n in range(1, length + 1):
+        if m[n] == 0:
+            zero_set.append(n)
+        elif m[n] > 1:
+            for _ in range(m[n] - 1):
+                exceed_set.append(n)
+
+    result = 0
+
+    while zero_set:
+        target_n = zero_set.pop()
+        exceed_n = exceed_set.pop()
+
+        result += abs(exceed_n - target_n)
+    print(result)
     return result
 
 
-print(func(125))
-print(func(42))
-print(func(634))
-print(func(32424))
+func([1, 2, 1])
+func([2, 1, 4, 4])
+func([6, 2, 3, 5, 6, 3])
+func([6, 2, 3, 5, 6, 6])
